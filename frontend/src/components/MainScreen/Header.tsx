07c6c6598871d42ca8abdb7d2767737687fdbd9a
sikorski1 <kacpersikora.praca@gmail.com>
2025-03-29 12:53:44 +0100

    fix: fix

--- frontend/src/components/MainScreen/Header.tsx
+++ frontend/src/components/MainScreen/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <header className="w-full bg-black">
-            <h1 className="uppercase">miasteczkomat</h1>
+        <header className="w-full bg-red-custom">
+            <h1 className="uppercase text-red-custom">miasteczkomat</h1>
         </header>
     )
 }
