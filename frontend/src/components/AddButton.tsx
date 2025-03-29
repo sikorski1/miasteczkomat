2d97adde0e812c04ec0ea555c8b8f77a865332d0
jplosko <jplosko@upv.edu.es>
2025-03-29 17:09:12 +0100

    feat: addprocudt funcionality

--- frontend/src/components/AddButton.tsx
+++ frontend/src/components/AddButton.tsx
@@ -1,9 +1,16 @@
 import { Plus } from "lucide-react";
 
-export default function AddButton() {
+type Props = {
+	handleSetModalOpen: (modalName: string) => void;
+};
+
+export default function AddButton({ handleSetModalOpen }: Props) {
 	return (
 		<div className="absolute absolute bottom-24 right-4">
-			<button className="size-16 bg-red rounded-full flex items-center justify-center">
+			<button
+				onClick={() => handleSetModalOpen("addProd")}
+				className="size-16 bg-red rounded-full flex items-center justify-center"
+			>
 				<Plus className="text-white text-2xl "></Plus>
 			</button>
 		</div>
--- frontend/src/components/MainScreen/AddProductModal.tsx
+++ frontend/src/components/MainScreen/AddProductModal.tsx
@@ -37,23 +37,34 @@ export default function AddProductModal() {
 	};
 
 	return (
-		<div className="w-full h-max bg-white shadow-lg rounded-2xl p-16">
-			<h2 className="text-xl font-semibold">Add New Product</h2>
+		<div className="w-full h-max bg-white shadow-lg rounded-2xl p-16 flex-col justify-center">
+			<p className=" text-3xl font-semibold mb-8 text-center">wystaw nowy produkt</p>
 			<form onSubmit={handleSubmit} className="space-y-4">
 				<Input
+					className={"p-2 border rounded-md divide-solid border-[#dfdfdf]"}
 					name="name"
 					value={product.name}
 					onChange={handleChange}
 					placeholder="nazwa"
 					required
 				/>
+				<div className="w-full flex items-center">
+					<label
+						htmlFor="file-upload"
+						className="w-full cursor-pointer rounded-lg bg-white text-black p-2 border rounded-md divide-solid border-[#dfdfdf] transition"
+					>
+						dodaj zdjęcie
+					</label>
+					<input
+						id="file-upload"
+						type="file"
+						className="hidden"
+						onChange={handleChange}
+					/>
+					<span className="text-gray-600">{product.photoUrl}</span>
+				</div>
 				<Input
-					name="photoUrl"
-					value={product.photoUrl}
-					onChange={handleChange}
-					placeholder="Photo URL"
-				/>
-				<Input
+					className={"w-full p-2 border rounded-md divide-solid border-[#dfdfdf]"}
 					name="price"
 					type="number"
 					value={product.price}
@@ -65,13 +76,14 @@ export default function AddProductModal() {
 					name="currency"
 					value={product.currency}
 					onChange={handleChange}
-					className="w-full p-2 border rounded-md"
+					className="w-full p-2 border rounded-md divide-solid border-[#dfdfdf]"
 				>
 					<option value="PLN">pln</option>
 					<option value="EUR">eur</option>
 					<option value="Waluta Studencka">waluta studencka</option>
 				</select>
 				<Textarea
+					className={"w-full p-2 border rounded-md divide-solid border-[#dfdfdf]"}
 					name="description"
 					value={product.description}
 					onChange={handleChange}
@@ -81,7 +93,7 @@ export default function AddProductModal() {
 					name="category"
 					value={product.category}
 					onChange={handleChange}
-					className="w-full p-2 border rounded-md"
+					className="w-full p-2 border rounded-md divide-solid border-[#dfdfdf]"
 				>
 					<option value="ELEKTRONIKA">elektronika</option>
 					<option value="ŻYWNOŚĆ">żywność</option>
@@ -102,14 +114,16 @@ export default function AddProductModal() {
 					name="actionType"
 					value={product.actionType}
 					onChange={handleChange}
-					className="w-full p-2 border rounded-md"
+					className="w-full p-2 border rounded-md divide-solid border-[#dfdfdf]"
 				>
 					<option value="SELL">sprzedaj</option>
 					<option value="RENT">pożycz</option>
 				</select>
-				<Button type="submit" className="pt-4 pb-4 pl-6 pr-6 bg-b-gray rounded-xl">
-					Add Product
-				</Button>
+				<div className="w-full flex justify-center">
+					<Button type="submit" className="pt-4 pb-4 pl-6 pr-6 bg-b-gray rounded-xl">
+						Add Product
+					</Button>
+				</div>
 			</form>
 		</div>
 	);
