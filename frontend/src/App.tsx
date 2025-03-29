3cdf1e0a79529e84ce535ad9abd8e9594b89f1f4
jplosko <jplosko@upv.edu.es>
2025-03-29 18:24:57 +0100

    feat: font update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Router from "./routing/Router";
+
+
 const queryClient = new QueryClient();
 function App() {
 	return (
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
+			fontFamily: {
+				lato: ["Lato", "sans-serif"],
+			},
 			colors: {
 				'white-custom': "#ffffff",
 				'gray-custom': "#6f6f6f",
